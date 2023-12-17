31a256840333836bc2d9c89b1fc4765750f001d5
Antonio Colagreco <92807691+antoniocolagreco@users.noreply.github.com>
2023-12-17 12:28:18 +0100

    Updates

--- component---src-pages-index-tsx-79c20f11c60918eccc74.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkadkaora_com=self.webpackChunkadkaora_com||[]).push([[691],{908:function(a,e,n){n.r(e);var o=n(1527);e.default=a=>{if("undefined"!=typeof window){const a="it",e="it,en".split(","),n=navigator.language.substring(0,2);e.indexOf(n.toLowerCase())>=0?window.location.replace(`adkaora-build/${n}`):window.location.replace(`adkaora-build/${a}`)}return(0,o.jsx)(o.Fragment,{})}}}]);
-//# sourceMappingURL=component---src-pages-index-tsx-79c20f11c60918eccc74.js.map
